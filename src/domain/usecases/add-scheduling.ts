b54ff5a5b0982f163e3381c28ace64913b9ace3f
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-09-04 18:36:02 -0300

    feat: Create Scheduling

---
+++ src/domain/usecases/add-scheduling.ts
@@ -0,0 +1,8 @@
+export interface AddSchedulingDto {
+    customer: string
+    note: string
+    schedules: Array<{date: string,experts_id: number[]}>
+}
+export interface AddScheduling {
+    add(data: AddSchedulingDto): Promise<string>
+}
---
+++ src/presentation/controllers/scheduling/create-scheduling.ts
@@ -0,0 +1,22 @@
+import { AddScheduling } from "../../../domain/usecases/add-scheduling";
+import { Controller } from "../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
+
+export class CreateScheduling implements Controller {
+    constructor (private readonly addScheduling: AddScheduling){}
+
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const request = httpRequest.body
+
+        if (!request.customer) return {statusCode: 400,data: "customer is required"}
+        if (!request.note) return {statusCode: 400,data: "note is required"}
+        if (!request.schedules) return {statusCode: 400,data: "schedules is required"}
+
+        const schedule_id = await this.addScheduling.add(request)
+
+        return {
+            statusCode: 200,
+            data: { schedule_id }
+        }
+    }
+}
